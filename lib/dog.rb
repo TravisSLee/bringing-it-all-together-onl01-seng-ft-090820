a7a260d687873ae46fc829974c2140edeed7769b
Travis Lee <travisslee223@gmail.com>
2020-10-10 21:40:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,13 @@ class Dog
     end.first
   end
   
-  def self.find_by_id
-    
+  def self.find_by_id(id)
+     sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
